d191c11d6f9ec8f6ee91368af0d4b747c50d3990
cday-fics <carson.day@icloud.com>
2016-01-31 15:36:51 -0500

    Changed version to 1.0!

--- raptor/src/raptor/pref/RaptorPreferenceStore.java
+++ raptor/src/raptor/pref/RaptorPreferenceStore.java
@@ -385,8 +385,8 @@ public class RaptorPreferenceStore extends PreferenceStore implements Preference
 		setDefault(ACTION_SEPARATOR_SEQUENCE, 400);
 
 		// App settings.
-		setDefault(APP_NAME, "Raptor 1.0 rc6");
-		putValue("app-version", "1.0 rc6");
+		setDefault(APP_NAME, "Raptor 1.0");
+		putValue("app-version", "1.0");
 		setDefault(APP_IS_SHOWING_CHESS_PIECE_UNICODE_CHARS, !OSUtils.isLikelyWindowsXP());
 		setDefault(APP_SASH_WIDTH, 8);
 		setDefault(APP_HOME_URL, "https://raptor-fics-interface.github.io/Raptor/");
